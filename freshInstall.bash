790c1708ec29926ecdd8d86bdc45f40a5e96bfd1
Andreas Mosti <andreas.mosti@gmail.com>
2013-09-03 21:45:42 +0200

    Edited freshInstall - script with less clutter.

--- freshInstall.bash
+++ freshInstall.bash
@@ -1,19 +1,25 @@
 #! /bin/bash
 
 function install() {
-sudo apt-get install -y $1 
+sudo apt-get install -y $1 >/dev/null 2>&1 
     }
 
-programs="build-essential vim tor nmap ssh curl apg smbclient htop git hydra make traceroute screen python-dev openjdk-7-jdk
+programs="build-essential vim tor  ssh curl  htop git make traceroute screen python-dev openjdk-7-jdk
 apache2 php5" 
 
-sudo apt-get update
+echo "Installing web-server essentials..."
+sudo apt-get update >/dev/null 2>&1 
 
 for program in $programs
-do
-    install $program
+do 
+    echo "Current package: $program" 
+    install $program 
+
 done 
+
+echo "Symlincing /www folder to /Vagrant folder..."
 
 rm -rf /var/www
 ln -fs /vagrant /var/www 
 
+echo "All done! go 'vagrant ssh'" 
