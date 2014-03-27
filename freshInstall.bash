d1e9d30809bbbdae9bb9f0b332803ec240d1408e
Andreas Mosti <andreas.mosti@gmail.com>
2014-03-27 23:14:17 +0100

    Added databasesetup

--- freshInstall.bash
+++ freshInstall.bash
@@ -4,10 +4,17 @@ function install() {
 sudo apt-get install -y $1 >/dev/null 2>&1 
     }
 
-programs="build-essential vim tor  ssh curl  htop git make traceroute screen python-dev openjdk-7-jdk
-apache2 php5" 
+programs="build-essential vim tor ssh curl  htop git make traceroute screen python-dev apache2 php5 mysql-server php5-mysql
+php5-mysqlnd php-mdb2-driver-mysql mysql-libs" 
+
+pass=$(</vagrant/mysqlpw.txt)
+
+ sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $pass"
+ sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $pass"
+
 
 echo "Installing web-server essentials..."
+
 sudo apt-get update >/dev/null 2>&1 
 
 for program in $programs
